cedcdc5dda63e85fd9909301c8bc7abf2e88b1cc
alvinlimassa <alvin.limassa@gmail.com>
2018-02-11 17:19:15 +0700

    read previous round + Unfinished attack
    attack not tested yet

--- Bot/Python3/bot.py
+++ Bot/Python3/bot.py
@@ -36,32 +36,116 @@ def output_shot(x, y):
     pass
 
 
+def attack_state(opponent_map1):
+    with open(os.path.join(output_path, game_state_file), 'r') as f_in:
+        state = json.load(f_in)
+
+    prevround = state["Round"] - 1
+    roundchar = "Round"
+    playerkey = state["PlayerMap"]["Owner"]["Key"]
+    prev_output_path = output_path[0:output_path.index(roundchar) + len(roundchar)]
+    if playerkey == "B":
+        prev_output_path = prev_output_path + " " + str(prevround) + "/B"
+    else:
+        prev_output_path = prev_output_path + " " + str(prevround) + "/A"
+
+    if state["Round"] == 1:
+        attackstatus = 0
+        firsthitx = -1
+        firsthity = -1
+    else:
+        with open(os.path.join(prev_output_path, game_state_file), 'r') as f_inx:
+            prevstate = json.load(f_inx)
+        with open(os.path.join(prev_output_path, attack_status), 'r') as f_inz:
+            prevstatus = f_inz.read().splitlines()
+        prevattack = prevstatus[0]
+        x,y = prevattack.split()
+        currentship = ship_count(state['OpponentMap']['Ships'])
+        for cell in opponent_map1:
+            if cell['Damaged'] and int(cell['X']) == int(x) and int(cell['Y']) == int(y):
+                if int(prevstatus[4]) == -1 and int(prevstatus[5]) == -1:
+                    attackstatus = 1
+                    firsthitx = int(x)
+                    firsthity = int(y)
+                elif int(prevstatus[2]) != int(currentship[0]) and int(currentship[1]) != int(state['Owner']['Ships']['ShotsHit']):
+                    attackstatus = 1
+                    firsthitx = int(prevstatus[4])
+                    firsthity = int(prevstatus[5])
+                elif int(prevstatus[2]) != int(currentship[0]) and int(currentship[1]) == int(state['Owner']['Ships']['ShotsHit']):
+                    attackstatus = 0
+                    firsthitx = -1
+                    firsthity = -1
+            elif cell['Missed'] and int(cell['X']) == int(x) and int(cell['Y']) == int(y):
+                if int(prevstatus[4]) == -1 and int(prevstatus[5]) == -1:
+                    attackstatus = 0
+                    firsthitx = -1
+                    firsthity = -1
+                elif int(prevstatus[4]) != -1 and int(prevstatus[5]) != -1:
+                    attackstatus = 1
+                    firsthitx = int(prevstatus[4])
+                    firsthity = int(prevstatus[5])
+            
+    return attackstatus, firsthitx, firsthity
+
 def fire_shot(opponent_map):
     # To send through a command please pass through the following <code>,<x>,<y>
     # Possible codes: 1 - Fireshot, 0 - Do Nothing (please pass through coordinates if
     #  code 1 is your choice)
 
-    """
-        * Randomize the place to shot in a chessboard pattern, prioritize center area first
-        * If it hits :
-            * If special shots is currentyl available, fire it.
-                * For every direction the shot hits, continue along that direction with normal shots.
-                * If it hits nothing, target untested direction with normal shots.
-                Afterwards, follow normal shots rule.
-            * Else,
-                * Try to shoot up, down, left, or right (randomized).
-                  If it doesn't hit, try another direction.
-                  If it hits, continue along that direction until the opponent's ship is destroyed.
-    """
-
+    #proses pengaksesan state sebelumnya
+    
+    attackstatus = attack_state(opponent_map)
     targets = []
-    for cell in opponent_map:
-        if not cell['Damaged'] and not cell['Missed']:
-            valid_cell = cell['X'], cell['Y']
-            targets.append(valid_cell)
-    target = random.choice(targets)
+    if map_size == 10 :
+	    for cell in opponent_map:
+	        if not cell['Damaged'] and not cell['Missed'] and (int(cell['X'])+ int(cell['Y']))%2 == 0:
+	            valid_cell = cell['X'], cell['Y']
+	            targets.append(valid_cell)
+    target = choice(targets)
     output_shot(*target)
+    targetx = target[0]
+    targety = target[1]
+    with open(os.path.join(output_path, game_state_file), 'r') as f_inxx:
+        state = json.load(f_inxx)
+    enemyboatcount = ship_count(state['OpponentMap']['Ships'])
+    countboat = enemyboatcount[0]
+    countsisa = enemyboatcount[1]
+    
+    
+    
+    with open(os.path.join(output_path, attack_status), 'w') as f_out:
+        f_out.write('{} {}'.format(targetx,targety))
+        f_out.write('\n')
+        f_out.write('{}'.format(attackstatus[0]))
+        f_out.write('\n')
+        f_out.write('{}'.format(countboat))
+        f_out.write('\n')
+        f_out.write('{}'.format(countsisa))
+        f_out.write('\n')
+        f_out.write('{}'.format(attackstatus[1]))
+        f_out.write('\n')
+        f_out.write('{}'.format(attackstatus[2]))
+
     return
+
+
+def ship_count(enemy_location):
+    count = 0
+    sisa = 0
+    for ship in enemy_location :
+        if not ship['Destroyed']:
+            count += 1
+        if ship['ShipType'] == 'Submarine' :
+            sisa += 3
+        if ship['ShipType'] == 'Battleship' :
+            sisa += 4
+        if ship['ShipType'] == 'Carrier' :
+            sisa += 5
+        if ship['ShipType'] == 'Cruiser' :
+            sisa += 3
+        if ship['ShipType'] == 'Destroyer':
+            sisa += 2
+    return count,sisa
 
 def InsertShipIntoMap(cmd,map):
     # Inserting a ship to the dummy map based on the command string
