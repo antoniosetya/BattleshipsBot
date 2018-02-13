697d3a3b73f90de0c5e8529c2fdbb4bc7ab3e901
antoniosetya <antonio_setya@hotmail.com>
2018-02-13 19:24:28 +0700

    Minor edits from Limassa

--- Bot/Python3/bot.py
+++ Bot/Python3/bot.py
@@ -178,7 +178,7 @@ def fire_shot(opponent_map):
                 valid_cell = cell['X'], cell['Y']
                 targets.append(valid_cell)
         elif int(attackstatus[0]) == 2:
-            if not cell['Damaged'] and not cell['Missed'] and int(attackstatus[0]) == 1 and int(cell['X']) >= int(attackstatus[4])-3 and int(cell['X']) <= int(attackstatus[4])+3 and int(cell['Y']) <= int(attackstatus[5])+3 and int(cell['Y']) >= int(attackstatus[5])-3:
+            if not cell['Damaged'] and not cell['Missed'] and int(attackstatus[0]) == 1 and int(cell['X']) >= int(attackstatus[1])-3 and int(cell['X']) <= int(attackstatus[1])+3 and int(cell['Y']) <= int(attackstatus[2])+3 and int(cell['Y']) >= int(attackstatus[2])-3:
                 valid_cell = cell['X'], cell['Y']
                 targets.append(valid_cell)
 
@@ -204,6 +204,14 @@ def fire_shot(opponent_map):
     target = random.choice(targets)
     with open(os.path.join(output_path, game_state_file), 'r') as f_inxx:
         state = json.load(f_inxx)
+
+    if map_size == 10:
+        energyround = 3
+    elif map_size ==14:
+        energyround = 4
+    else:
+        energyround = 2
+    seekerenergy = energyround * 10
 
     output_shot(*target)
     targetx = target[0]
